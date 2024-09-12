ee245c7daf042791d7a79f26201c27e66be616dd
Iguux <164428739+iguuuuz@users.noreply.github.com>
2024-09-12 19:47:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
-class Aluno:
-    def __init__(self, nome, nota1=0.0, nota2=0.0):
-        self.__nome = nome
+class aluno :
+    def __init__(self,nome, nota1, nota2):
+      
+        self.__Nome = nome
         self.__nota1 = nota1
         self.__nota2 = nota2
-
-    def set_nome(self, nome):
-        self.__nome = nome
+    
+    def set_nome(self, novo_nome):
+        self.__Nome = novo_nome
 
     def get_nome(self):
-        return self.__nome
-
-    def set_nota1(self, nota1):
-        if 0.0 <= nota1 <= 10.0:  
-            self.__nota1 = nota1
-        else:
-            raise ValueError("A nota 1 deve estar entre 0 e 10")
+        return self.__Nome
+       
+    def set_nota1(self, nova_nota1):
+        self.__nota1 = nova_nota1
 
     def get_nota1(self):
         return self.__nota1
 
-    def set_nota2(self, nota2):
-        if 0.0 <= nota2 <= 10.0:  
-            self.__nota2 = nota2
-        else:
-            raise ValueError("A nota 2 deve estar entre 0 e 10")
+    def set_nota2(self, nova_nota2):
+        self.__nota2 = nova_nota2
 
     def get_nota2(self):
         return self.__nota2
-
+   
+   
     def calcular_media(self):
-        return (self.__nota1 + self.__nota2) / 2
+        return (self.__nota1 + self.__nota2) / 
+    def exibir_informacoes(self):
+        media = self.calcular_media()
+        print(f"Nome: {self.__Nome}, média: {ou media:.2f} "o)
+
+aluno1 = aluno("higor", 8 , 9)
+aluno1.exibir_informacoes()
+
+aluno1.set_nome("lucas")
+aluno1.set_nota1(10)
+aluno1.set_nota2(7)
+aluno1.exibir_informacoes()
+
+aluno1.set_nome("juliana")
+aluno1.set_nota1(8)
+aluno1.set_nota2(3)
+aluno1.exibir_informacoes()
